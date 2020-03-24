266ab7b8e7888d2c8959f83d3ba66da6a60b07d2
Monika <monikasharma26081991@gmail.com>
2020-03-24 19:40:48 -0400

    Initial Commit

---
+++ C0774174_MAD3115_2_ClassExercise14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0774174_MAD3115_2_ClassExercise14
+//
+//  Created by S@i on 2020-03-24.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
